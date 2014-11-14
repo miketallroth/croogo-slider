e80d724310c1b18ee7c67e9600e7278720985479
Mike Tallroth <mike.tallroth@gmail.com>
2014-11-13 23:46:55 -0600

    functionality merged into croogo 2.2

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,26 +1,3 @@
 <?php
 
     Croogo::hookAdminTab('Links/admin_edit', 'Slider', 'NivoSlider.admin_tab_link');
-
-
-/*
-    $linkChoosers['attachments'] = array(
-        'title' => 'attachments',
-        'description' => 'attachments',
-        'url' => array(
-            'plugin' => 'file_manager',
-            'controller' => 'attachments',
-            'action' => 'index',
-            '?' => array(
-                'mime_type' => 'image/jpeg',
-                'chooser' => 1,
-                'KeepThis' => true,
-                'TB_iframe' => true,
-                'height' => 400,
-                'width' => 600
-            )
-        )
-    );
-    Croogo::mergeConfig('Menus.linkChoosers', $linkChoosers);
-
- */
